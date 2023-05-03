bf2ab0ef99e1de7f1fa85b4208888174ab7dbcd0
Syed Zaka ur Rehman <zrehman@dateitup.lan>
2023-05-03 03:06:59 -0700

    First push Jenki

---
+++ src/main/java/com/example/Jenki/JenkiApplication.java
@@ -0,0 +1,14 @@
+package com.example.Jenki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkiApplication.class, args);
+		System.out.println("Hi");
+	}
+
+}
---
+++ src/test/java/com/example/Jenki/JenkiApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.example.Jenki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
